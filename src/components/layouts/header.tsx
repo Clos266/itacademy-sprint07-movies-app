fb6e04e212e0f2f930d646494653d48adc259f44
Clos266 <clos266@gmail.com>
2025-07-31 17:04:12 +0200

    add logo images for header and full display

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,6 +1,9 @@
+import logo from "../../assets/logo-tmdb-header.svg";
+
 export default function Header() {
   return (
     <header>
+      <img src={logo} alt="Logo" />
       <h1>MovieApp</h1>
     </header>
   );
